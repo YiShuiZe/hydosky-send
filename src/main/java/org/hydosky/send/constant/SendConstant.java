1a34d5cfa19f52de665e6360431bfb5385fc03f8
YangSha <17809279506@163.com>
2019-12-21 23:48:24 +0800

    系统常量池

---
+++ src/main/java/org/hydosky/send/constant/SendConstant.java
@@ -0,0 +1,24 @@
+package org.hydosky.send.constant;
+
+/**
+ * <h3>hydosky-send</h3>
+ * <p>
+ *     系统常量池
+ * </p>
+ * Created by yang on 19-12-1 下午1:57
+ * updated by yang on 19-12-1 下午1:57
+ */
+public interface SendConstant {
+    /**
+     * 默认为空消息
+     */
+    String DEFAULT_NULL_MESSAGE = "暂无承载数据";
+    /**
+     * 默认成功消息
+     */
+    String DEFAULT_SUCCESS_MESSAGE = "操作成功";
+    /**
+     * 默认失败消息
+     */
+    String DEFAULT_FAILURE_MESSAGE = "操作失败";
+}
